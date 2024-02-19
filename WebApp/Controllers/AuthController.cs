5564eb5adbf79db4004cee4c5ca23db857eb9eef
Tim <tim.martensson92@gmail.com>
2024-02-19 14:31:52 +0100

    added auth

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult SignUp()
+    {
+        ViewData["Title"] = "Sign Up.";
+        return View();
+    }
+
+    //public IActionResult SignIn()
+    //{
+    //    ViewData["Title"] = "Sign In.";
+    //    return View();
+    //}
+
+
+    //public new IActionResult SignOut()
+    //{
+    //    return RedirectToAction("Index", "Home");
+    //}
+}
