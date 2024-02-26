bce4532cc6f667172abbe79d3c29422454853c42
Tim <tim.martensson92@gmail.com>
2024-02-26 09:08:21 +0100

    validation done

--- WebApp/Models/Components/SignUpModel.cs
+++ WebApp/Models/Components/SignUpModel.cs
@@ -5,35 +5,36 @@ namespace WebApp.Models.Components;
 public class SignUpModel
 {
     [Display(Name = "First Name", Prompt = "Enter your first name", Order = 0)]
-    [Required(ErrorMessage = "first name is required")]
+    [Required(ErrorMessage = "Enter first name")]
+    [MinLength(2, ErrorMessage = "First name is too short")]
     public string FirstName { get; set; } = null!;
 
     [Display(Name = "Last Name", Prompt = "Enter your last name", Order = 1)]
-    [Required(ErrorMessage = "last name is required")]
+    [Required(ErrorMessage = "Enter last name")]
+    [MinLength(2, ErrorMessage = "Last name is too short")]
     public string LastName { get; set; } = null!;
 
     [DataType(DataType.EmailAddress)]
     [Display(Name = "Email", Prompt = "Enter your email", Order = 2)]
-    [EmailAddress(ErrorMessage = "Invalid email")]
-    [Required(ErrorMessage = "Email is required")]
+    [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email")]
+    [Required(ErrorMessage = "Invalid email")]
     public string Email { get; set; } = null!;
 
     [Display(Name = "Password", Prompt = "Enter your password", Order = 3)]
-    [Required(ErrorMessage = "Password is required")]
-    [StringLength(50, ErrorMessage = "The password must be at least 8 characters long.", MinimumLength = 8)]
+    [Required(ErrorMessage = "Invalid password")]
+    [StringLength(50, ErrorMessage = "The password must be at least 8 characters", MinimumLength = 8)]
     [DataType(DataType.Password)]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,50}$", ErrorMessage = "Password must contain at least one uppercase letter, one number, and one special character.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,50}$", ErrorMessage = "Password needs one uppercase letter, one number and special character")]
     public string Password { get; set; } = null!;
 
     [Display(Name = "Confirm Password", Prompt = "Confirm your password", Order = 4)]
-    [Required(ErrorMessage = "Password confirmation is required")]
+    [Required(ErrorMessage = "Password does not match")]
     [DataType(DataType.Password)]
     [Compare("Password", ErrorMessage = "Passwords do not match")]
     public string ConfirmPassword { get; set; } = null!;
 
     [Display(Name = "I agree to the terms and conditions", Order = 5)]
-    [Range(typeof(bool), "true", "true", ErrorMessage = "You must agree to the terms and conditions")]
-    //[CheckBoxRequired(ErrorMessage = "You must agree to the terms and conditions")]
+    [Range(typeof(bool), "true", "true", ErrorMessage = "You must agree to the terms & conditions to sell your soul to our dark lord")]
     public bool TermsAndConditions { get; set; } = false;
 
 }
@@ -46,16 +47,5 @@ public class SignUpModel
 //    }
 //}
 
-//public class CheckBoxRequired : ValidationAttribute, IClientModelValidator
-//{
-//    public void AddValidation(ClientModelValidationContext context)
-//    {
-//        throw new NotImplementedException();
-//    }
 
-//    public override bool IsValid(object value)
-//    {
-//        return value is bool boolean && boolean;
-//    }
-//}
 
