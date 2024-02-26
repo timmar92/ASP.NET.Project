23a48f314e34cf83004f003f3b6dc746b041adf6
Tim <tim.martensson92@gmail.com>
2024-02-26 15:12:55 +0100

    started building infrastructure

---
+++ Infrastructure/Repositories/Repo.cs
@@ -0,0 +1,127 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Factories;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories;
+
+public abstract class Repo<TEntity>(DataContext context) where TEntity : class
+{
+    private readonly DataContext _context = context;
+
+    public virtual async Task<ResponseResult> CreateOneAsync(TEntity entity)
+    {
+        try
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return ResponseFactory.Ok(entity);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> GetAllAsync()
+    {
+        try
+        {
+            IEnumerable<TEntity> result = await _context.Set<TEntity>().ToListAsync();
+            return ResponseFactory.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> GetOneAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result == null)
+            {
+                return ResponseFactory.NotFound();
+            }
+            else
+            {
+                return ResponseFactory.Ok(result);
+            }
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> UpdateOneAsync(Expression<Func<TEntity, bool>> predicate, TEntity updatedEntity)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result != null)
+            {
+                _context.Entry(result).CurrentValues.SetValues(updatedEntity);
+                await _context.SaveChangesAsync();
+                return ResponseFactory.Ok(result);
+            }
+            else
+            {
+                return ResponseFactory.NotFound();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> DeleteOneAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            if (result != null)
+            {
+                _context.Set<TEntity>().Remove(result);
+                await _context.SaveChangesAsync();
+                return ResponseFactory.Ok("Successfully removed");
+            }
+            else
+            {
+                return ResponseFactory.NotFound();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public virtual async Task<ResponseResult> AlreadyExistsAsync(Expression<Func<TEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<TEntity>().AnyAsync(predicate);
+            if (result)
+            {
+                return ResponseFactory.Exists();
+            }
+            else
+            {
+                return ResponseFactory.NotFound();
+            }
+
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Infrastructure.Factories;
+using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository(DataContext context) : Repo<UserEntity>(context)
+{
+    private readonly DataContext _context = context;
+
+    public override async Task<ResponseResult> GetAllAsync()
+    {
+        try
+        {
+            IEnumerable<UserEntity> result = await _context.Users
+                .Include(x => x.Address)
+                .ToListAsync();
+
+            return ResponseFactory.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+
+    public override async Task<ResponseResult> GetOneAsync(Expression<Func<UserEntity, bool>> predicate)
+    {
+        try
+        {
+            var result = await _context.Set<UserEntity>()
+                .Include(x => x.Address)
+                .FirstOrDefaultAsync(predicate);
+
+            if (result == null)
+            {
+                return ResponseFactory.NotFound();
+            }
+            else
+            {
+                return ResponseFactory.Ok(result);
+            }
+        }
+        catch (Exception ex)
+        {
+            return ResponseFactory.Error(ex.Message);
+        }
+    }
+}
